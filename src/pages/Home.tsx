b6c9837197c6c7a4d9d597b3875f546aae836d71
Olubummo Faith <59509795+Leye5555@users.noreply.github.com>
2024-12-02 10:31:40 +0100

    broken update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,14 @@ import Main from "../components/Main";
 import { Outlet } from "react-router-dom";
 
 const Home = () => {
+  {
+    {
+      {
+        {
+        }
+      }
+    }
+  }
   return (
     <div className="w-screen h-screen relative">
       <Header />
